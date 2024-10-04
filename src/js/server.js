10d5e4704343542ae05da416af0e62b64ff1e4fd
r0mankeys <89408807+r0mankeys@users.noreply.github.com>
2024-10-04 20:41:10 +0100

    Make Express aware of assets folder

--- src/js/server.js
+++ src/js/server.js
@@ -4,11 +4,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("src"));
+app.use(express.static("assets"));
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
+	res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
